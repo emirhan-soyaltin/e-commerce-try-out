b326fddf5dd1906340056f76f32391e2eabe6016
emirjaqem <emirjaqem@gmail.com>
2023-03-14 23:27:56 +0300

    project started

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,81 @@
+import { Search } from '@mui/icons-material'
+import { Badge } from '@mui/material'
+import React from 'react'
+import styled from 'styled-components'
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
+const Container = styled.div`
+height: 60px;
+`
+const SearchContainer = styled.div`
+border: 0.5px solid lightgray;
+display: flex;
+align-items : center;
+margin-left: 25px;
+padding: 5px;
+`
+const Input = styled.input`
+border: none;
+`
+const Logo = styled.h1`
+font-weight: bold
+`
+
+const Wrapper = styled.div`
+padding: 10px 20px;
+display: flex;
+align-items: center;
+justify-content: space-between;
+`
+const Left =styled.div`
+flex: 1;
+display: flex;
+align-items : center;
+`
+const Langueage = styled.span`
+ font-size: 14px;
+ cursor: pointer;
+`
+
+const Center =styled.div`
+flex: 1;
+text-align: center;
+`
+const Right =styled.div`
+flex: 1;
+display: flex;
+align-items: center;
+justify-content: flex-end;
+`
+const MenuItem = styled.div`
+font-size: 14px;
+cursor: pointer;
+margin-left:25px;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Left>
+                <Langueage>EN</Langueage>
+                <SearchContainer>
+                  <Input/>
+                    <Search style={{color:"gray", fontSize:"24px"}}/>
+                </SearchContainer>
+                </Left>
+            <Center><Logo>EMIR.</Logo></Center>
+            <Right>
+              <MenuItem>REGISTER</MenuItem>
+              <MenuItem>SIGN IN</MenuItem>
+              <MenuItem><Badge badgeContent={4} color="primary">
+      <ShoppingCartOutlinedIcon />
+    </Badge></MenuItem>
+            </Right>
+        </Wrapper>
+
+    </Container>
+  )
+}
+
+export default Navbar
