00a0365f562ba2e97848d9f20edb2d9c2ab129d3
emirjaqem <emirjaqem@gmail.com>
2023-03-27 00:01:11 +0300

    components updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,28 @@
-import { Facebook, Instagram, Pinterest, Twitter } from "@mui/icons-material"
+import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from "@mui/icons-material"
 import styled from "styled-components"
 
 const Container = styled.div`
 display:flex ;
 `
 const Logo = styled.h1``
-const Desc = styled.p``
-const SocialContainer = styled.div``
-const SocialIcon = styled.div``
+const Desc = styled.p`
+margin: 20px 0px;
+`
+const SocialContainer = styled.div`
+display: flex;
+`
+const SocialIcon = styled.div`
+width: 40px;
+height: 40px;
+border-radius: 50%;
+color:#fff;
+background-color: #${props => props.color};
+display: flex;
+align-items: center;
+justify-content: center;
+cursor: pointer;
+margin-right: 20px;
+`
 
 
 const Left = styled.div`
@@ -18,9 +33,35 @@ padding: 20px;
 `
 const Center = styled.div`
 flex:1;
+padding: 20px;
 `
 const Right = styled.div`
 flex:1;
+padding: 20px;
+`
+
+const Title = styled.h3`
+margin-bottom: 30px;
+`
+const List = styled.ul`
+margin: 0;
+padding: 0;
+list-style: none;
+display: flex;
+flex-wrap: wrap;
+`
+
+const ListItem = styled.li`
+width: 50%;
+margin-bottom: 10px;
+`
+const ContactItem = styled.div`
+margin-bottom: 20px;
+display: flex;
+align-items: center;
+`
+const Payment = styled.img`
+width: 50%;
 `
 
 const Footer = () => {
@@ -32,23 +73,49 @@ const Footer = () => {
             There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
         </Desc>
         <SocialContainer>
-            <SocialIcon>
+            <SocialIcon   color='3B5999'>
                 <Facebook />
             </SocialIcon>
-            <SocialIcon>
+            <SocialIcon color='E4405F'>
                 <Instagram />
-            </SocialIcon>
-            <SocialIcon>
+            </SocialIcon >
+            <SocialIcon color='55ACEE'>
                 <Twitter />
             </SocialIcon>
-            <SocialIcon>
+            <SocialIcon  color='E60023'>
                 <Pinterest />
             </SocialIcon>
-            
         </SocialContainer>
-       </Left>
-       <Center></Center>
-       <Right></Right>
+    </Left>
+       <Center>
+        <Title>Useful Links</Title>
+        <List>
+            <ListItem>Home</ListItem>
+            <ListItem>Cart</ListItem>
+            <ListItem>Man Fashion</ListItem>
+            <ListItem>Women Fashion</ListItem>
+            <ListItem>Accessories</ListItem>
+            <ListItem>Order Tracking</ListItem>
+            <ListItem>Wishlist</ListItem>
+            <ListItem>Terms</ListItem>
+        </List>
+    </Center>
+       <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+            <Room style={{marginRight:'10px'}} />
+            622 Karşiyaka , Izmir Turkey
+        </ContactItem>
+        <ContactItem>
+            <Phone style={{marginRight:'10px'}} />
+            +1 234 56 78
+        </ContactItem>
+        <ContactItem>
+            <MailOutline style={{marginRight:'10px'}} />
+            contact@emir.dev
+        </ContactItem>
+        <Payment src='https://i.ibb.co/Qfvn4z6/payment.png' />
+       </Right>
     </Container>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,62 +3,76 @@ import { Badge } from '@mui/material'
 import React from 'react'
 import styled from 'styled-components'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { mobile } from "./resposive";
 
 const Container = styled.div`
-height: 60px;
-`
-const SearchContainer = styled.div`
-border: 0.5px solid lightgray;
-display: flex;
-align-items : center;
-margin-left: 25px;
-padding: 5px;
-`
-const Input = styled.input`
-border: none;
-`
-const Logo = styled.h1`
-font-weight: bold
-`
+  height: 60px;
+  ${mobile({ height: "50px" })}
+`;
 
 const Wrapper = styled.div`
-padding: 10px 20px;
-display: flex;
-align-items: center;
-justify-content: space-between;
-`
-const Left =styled.div`
-flex: 1;
-display: flex;
-align-items : center;
-`
-const Langueage = styled.span`
- font-size: 14px;
- cursor: pointer;
-`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
+`;
 
-const Center =styled.div`
-flex: 1;
-text-align: center;
-`
-const Right =styled.div`
-flex: 1;
-display: flex;
-align-items: center;
-justify-content: flex-end;
-`
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+  ${mobile({ display: "none" })}
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+  ${mobile({ width: "50px" })}
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })}
+`;
+
 const MenuItem = styled.div`
-font-size: 14px;
-cursor: pointer;
-margin-left:25px;
-`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
+`;
 
 const Navbar = () => {
   return (
     <Container>
         <Wrapper>
             <Left>
-                <Langueage>EN</Langueage>
+                <Language>EN</Language>
                 <SearchContainer>
                   <Input/>
                     <Search style={{color:"gray", fontSize:"24px"}}/>
